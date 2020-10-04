9392b613057db240d220debad59de3d20471b0e5
Tmvaz <31070998+tmvaz@users.noreply.github.com>
2020-10-04 10:34:33 +0100

    Add files via upload

--- Problem 2 - Project Euler.py
+++ Problem 2 - Project Euler.py
@@ -1,30 +1,25 @@
 # PROJECT EULER - PROBLEM 2 (SOLUTION PROPOSAL)
 
-# this version for the fibonacci function uses a rudimentary form of "memoizing"
-# it calculates a number for the fibonacci series - and if that number was already
-# calculated, it doesn't calculate it again
-series = [0, 1]
+# this version for the fibonacci function
+# uses a rudimentary form of "memoizing - without a dictionary
+series = [1, 2]
 def fibo(n):
     if n > 0:
-        if n<=2:
+        if n<3:
             return series[n-1]
-        elif n < len(series):
+        elif n <= len(series):
             return series[n-1]
         else:
             series.append(fibo(n-1) + fibo(n-2))
             return series[-1]
 
-print(fibom(3), memo)
-""" not finished yet
-i = 0
+i = 1
 res = 0
-while(True):
-    z = fibonacci(i)
+z = 0
+while(z < 4000000):
+    z = fibo(i)
     if z%2 == 0:
         res = res + z
-    elif z > 4000000:
-        break
     i = i + 1
 
 print(res)
-"""
